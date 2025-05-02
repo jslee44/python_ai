8509f2ebec8e654ea28ce467afb677b119e3934a
jslee44 <junseogi44@gmail.com>
2025-05-02 13:58:49 +0900

    Update GDP.py

--- GDP.py
+++ GDP.py
@@ -5,5 +5,6 @@ gdp=[300, 543, 1075, 2862, 5979, 10289, 14958, 18320, 21448]
 
 plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
 plt.title('GDP')
+plt.xlabel('year')
 plt.ylabel('$')
 plt.show()
