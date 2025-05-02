7e23c1c71c5ebd4ae1e5aecd3fad997e11cd9fc6
jslee44 <junseogi44@gmail.com>
2025-05-02 15:00:07 +0900

    Create GDP_1.py

---
+++ GDP_1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, 2030]
+gdp = [300, 543, 1075, 2862, 5979, 10289, 14958, 18320, 21448]
+
+plt.plot(years, gdp, color='r', marker='*', linestyle='dotted')
+plt.title('GDP')
+plt.xlabel('Year')
+plt.ylabel('GDP ($ Billion)')
+plt.gca().invert_xaxis()
+plt.show()
