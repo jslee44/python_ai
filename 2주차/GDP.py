86b46d0a0973c9c2fa5a96be0a4555e82a2ed574
jslee44 <junseogi44@gmail.com>
2025-05-03 15:59:57 +0900

    Create GDP.py

---
+++ 2주차/GDP.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+years=[1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, 2030]
+gdp=[300, 543, 1075, 2862, 5979, 10289, 14958, 18320, 21448]
+
+plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+plt.title('GDP')
+plt.xlabel('year')
+plt.ylabel('$')
+plt.show()
