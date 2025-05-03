e642fc0c06951bef85e3994e0ed0c6d70a7fa16e
jslee44 <junseogi44@gmail.com>
2025-05-03 15:09:09 +0900

    Create chart_1.py

---
+++ chart_1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y1 = [2, 3, 5, 7, 11]
+y2 = [1, 4, 6, 8, 10]
+y3 = [3, 5, 2, 6, 9]
+
+plt.plot(x, y1, marker='o', label='Series A')
+plt.plot(x, y2, marker='*', label='Series B')
+plt.plot(x, y3, marker='^', label='Series C')
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend(bbox_to_anchor=(-0.1,0.9))
+plt.show()
