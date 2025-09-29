4c5e64652c7af72906300d4779fe0e9df8f37eaa
jslee44 <junseogi44@gmail.com>
2025-09-29 14:25:29 +0900

    Create sort.py

---
+++ 5주차/sort.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.DataFrame({'name':['Jessica','Liam','Sophia','Ryan','Alex'],
+                   'test':[45,30,40,37,48],
+                   'assign1':[20,17,22,18,24],
+                   'assign2':[19,14,18,15,25]})
+
+df['sum'] = df['test']+df['assign1']+df['assign2']
+sort_df = df.sort_values('sum', ascending=False)
+print(sort_df)
